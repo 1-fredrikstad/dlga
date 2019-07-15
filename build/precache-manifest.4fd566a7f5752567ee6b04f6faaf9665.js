04514468820ce6ee48cac2ad2a7d4f381e7d150d
Dag Frode Solberg <dag.frode.solberg@gmail.com>
2019-07-15 15:45:46 +0200

    new build

---
+++ build/precache-manifest.4fd566a7f5752567ee6b04f6faaf9665.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da93a6f2ed2a29b4cd5e41b6f3be762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d447b3996acb1dfd4c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "97bd67c485db6ab25733",
+    "url": "/static/js/2.a8578e7e.chunk.js"
+  },
+  {
+    "revision": "70d447b3996acb1dfd4c",
+    "url": "/static/js/main.f21ddce4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
